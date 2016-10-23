72a57e2256aa54548709dd5fc9ce00d60e5af950
Dean Armada <deanarmada@gmail.com>
2016-10-23 15:35:30 +0800

    Model Manager Improvement

--- project/sample_app/models.py
+++ project/sample_app/models.py
@@ -3,13 +3,13 @@ from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
 from django.db import models
 
-from core.model_managers import GetOrNoneManager
+from core.model_managers import GetManager
 
 # Create your models here.
 class Artist(models.Model):
 	name = models.CharField(max_length=255)
 	age = models.PositiveSmallIntegerField()
-	objects = GetOrNoneManager()
+	objects = GetManager()
 
 	# https://docs.djangoproject.com/es/1.9/ref/models/instances/#get-absolute-url
 	# Used to pass a location header URL upon post
